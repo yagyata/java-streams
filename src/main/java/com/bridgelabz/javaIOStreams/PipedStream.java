a4f44ff78c2f6662a569dddbe7456797ceb373f9
Yagyata <yagyatasharmaa@gmail.com>
2025-03-31 22:46:20 +0530

    [Yagyata] ADD: Piped Streams - Inter-Thread Communication

---
+++ src/main/java/com/bridgelabz/javaIOStreams/PipedStream.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.javaIOStreams;
+import java.io.*;
+
+class WriterThread extends Thread {
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    public void run() {
+        try {
+            String message = "Hello everyone!";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.out.println("WriterThread Error: " + e.getMessage());
+        }
+    }
+}
+
+class ReaderThread extends Thread {
+    private PipedInputStream pis;
+
+    public ReaderThread(PipedInputStream pis) {
+        this.pis = pis;
+    }
+
+    public void run() {
+        try {
+            int data;
+            while ((data = pis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            pis.close();
+        } catch (IOException e) {
+            System.out.println("ReaderThread Error: " + e.getMessage());
+        }
+    }
+}
+
+public class PipedStream {
+    public static void main(String[] args) {
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            writer.start();
+            reader.start();
+        } catch (IOException e) {
+            System.out.println("Error initializing pipes: " + e.getMessage());
+        }
+    }
+}
